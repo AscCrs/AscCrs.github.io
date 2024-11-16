acf4064293f137873cc7fd7b273d5258a1a4f050
AscCrs <cristoe2003@gmail.com>
2024-11-16 02:22:19 -0600

    Change vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/<cris-dev.github.io>'
+  base: "https://asccrs.github.io/cris-dev.github.io/"
 })
